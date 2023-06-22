d9b2bd96d96bf8735be07164bcc445f7859af6b5
kshitijkhedekar25 <kshitijkhedekar025gmail.com>
2023-06-23 00:57:51 +0530

    Read More button brings user to new tab now, added go back to home link on the show page

--- src/components/shows/ShowCard.jsx
+++ src/components/shows/ShowCard.jsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom';
 const ShowCard = ({ name, image, id, summary }) => {
   const summaryStripped = summary
     ? summary.split('').slice(0, 10).join('').replace(/<.+?>/g, '')
@@ -12,7 +11,9 @@ const ShowCard = ({ name, image, id, summary }) => {
       <h1>{name}</h1>
       <p>{summaryStripped}</p>
       <div>
-        <Link to={`/show/${id}`}>Read more</Link>
+        <a href={`/show/${id}`} target="_blank" rel="noreferrer">
+          Read more
+        </a>
         <button type="button">Star me</button>
       </div>
     </div>
